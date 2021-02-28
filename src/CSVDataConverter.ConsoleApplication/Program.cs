1b8c080ea512f7c1d04d2b2bd18bfb4dd2d62da2
shmuel <samuelwine@gmail.com>
2021-02-28 14:01:55 +0000

    upgrade solution to take input from a csv file

--- src/CSVDataConverter.ConsoleApplication/Program.cs
+++ src/CSVDataConverter.ConsoleApplication/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Xml.Linq;
 
@@ -9,24 +11,34 @@ namespace CSVDataConverter.ConsoleApplication
     class Program
     {
         static void Main(string[] args)
-        {
-            string csvString = "name,address";
-            string[] stringArray = csvString.Split(",");
-            dynamic interimObject = new ExpandoObject();
-            ((IDictionary<string, object>)interimObject)[stringArray[0]] = "Dave";
-            ((IDictionary<string, object>)interimObject)[stringArray[1]] = "Tewkesbury";
+        {   Console.WriteLine("Please enter file path:");
+            var filepath = Console.ReadLine();
+            string[] properties = File.ReadAllLines(filepath).Take(1).Single().Split(",");
+            var dataLines = File.ReadAllLines(filepath).Skip(1);
+            var list = new List<ExpandoObject>();
+            foreach (var dataline in dataLines)
+            {
+                string[] bits = dataline.Split(",");
+                dynamic interimObject = new ExpandoObject();
+                for (int i = 0; i < bits.Length; i++)
+                {
+                    ((IDictionary<string, object>)interimObject)[properties[i]] = bits[i];
+                }
+                list.Add(interimObject);
+            }
 
             //JSON serialization
-            var jsonOutput = JsonSerializer.Serialize(interimObject);
+            var jsonOutput = JsonSerializer.Serialize(list);
             Console.WriteLine(jsonOutput);
 
 
             //XML serialization
-            foreach (var item in interimObject as IDictionary<string, object>)
-            {
-                var xmlString = new XElement(item.Key, item.Value);
-                Console.WriteLine(xmlString);
-            }
+            foreach (var expando in list)
+                foreach (var item in expando as IDictionary<string, object>)
+                {
+                    var xmlString = new XElement(item.Key, item.Value);
+                    Console.WriteLine(xmlString);
+                }
         }
     }
 }
